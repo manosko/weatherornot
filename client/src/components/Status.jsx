69fa6725953db0c62c00226680e79c32634f779c
manosko <32654968+manosko@users.noreply.github.com>
2018-03-05 14:13:51 -0500

    hello

--- client/src/components/Status.jsx
+++ client/src/components/Status.jsx
@@ -1,70 +1,82 @@
+// pre-quentin
+
 import React from 'react';
 import ReactAnimatedWeather from 'react-animated-weather';
+
 
 class Status extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      status: [],
+      status: 'clear',
     };
-    this.getWeather = this.getWeather.bind(this);
   }
 
-  componentWillReceiveProps(nextProps) { // requires nextProps to see the new props (see React docs)
-    let newProps = Object.assign({}, nextProps);
-    if (nextProps.places.length === 0) {
-      newProps.places = this.props.places;
-    }
-    console.log("this.props.place>>>>>>>>", this.props.places);
-    console.log('COMPWILLPROPS   ', 'commutes', newProps.commutes, 'places', newProps.places)
-    if (newProps.commutes && newProps.commutes.length > 0) {
-      console.log('running')
-      this.getWeather(newProps);
-    }
-  }
+  componentWillMount() {
+    this.setState(
+      {
+        status:
+      this.props.commutes.reduce((acc, commute) => {
+        if (acc !== 'snow') {
+        // for the origin of each commute
+          const orign = this.props.places.find(place => place.id === commute.origin.id);
 
-  getWeather(props) { // props about to be passed into the component
-    let statusArr = [];
-    props.commutes.forEach((commute) => {
-      console.log('checking origin');
-      // for the origin of each commute
-      const origin = props.places.find(place => place.id === commute.origin.id);
+          const departureHour = origin.weather.hourly.data.find(hour =>
+          // find hour that describes departure time
+          // 3600 seconds in hour
+            (hour.time > commute.departure - 1800 && hour.time > commute.departure + 1800), // ???
+          );
 
-      const departureHour = origin.weather.hourly.data.find((hour) => {
-        // find hour that describes departure time
-        // 3600 seconds in hour
-        const hourUNIX = hour.time;
-        const commuteUNIX = Math.floor(new Date(commute.departure).getTime() / 1000);
+          // check the coresponding place's weather
+          if (acc === 'clear') {
+            if (departureHour.icon === 'rain' || departureHour.icon === 'snow') {
+              return departureHour.icon;
+            }
+            return acc;
+          } else if (acc === 'rain') {
+            if (departureHour.icon === 'snow') {
+              return departureHour.icon;
+            }
+            return acc;
+          }
+          return acc;
+        }
+      }, 'clear'),
+      },
 
-        // console.log('hour: ', hourUNIX, '  comm:', commuteUNIX);
+      () => {
+        if (this.state.status === 'clear' || this.state.status === 'rain') {
+          this.setState({
+            status:
+            this.props.commutes.reduce((acc, commute) => {
+              if (acc !== 'snow') {
+                // for the destination of each commute
+                const destination = this.props.places.find(place => place.id === commute.destination.id);
 
+                const arrivalHour = destination.weather.hourly.data.find(hour =>
+                  // find hour that describes arrival time
+                  // 3600 seconds in hour
+                  (hour.time > commute.arrival - 1800 && hour.time > commute.arrival + 1800));
 
-        return (hourUNIX > commuteUNIX - 1800 && hourUNIX > commuteUNIX + 1800); // ???
-      });
-      console.log(departureHour)
-      statusArr.push(departureHour.icon);
-    });
-
-    props.commutes.forEach((commute) => {
-      console.log('checking destination');
-      // for the destination of each commute
-      const destination = props.places.find(place => place.id === commute.destination.id);
-
-      const arrivalHour = destination.weather.hourly.data.find((hour) => {
-        // find hour that describes arrival time
-        // 3600 seconds in hour
-        const hourUNIX = hour.time;
-        const commuteUNIX = Math.floor(new Date(commute.departure).getTime() / 1000);
-
-        console.log('hour: ', hourUNIX, '  comm:', commuteUNIX);
-
-        return (hourUNIX > commuteUNIX - 1800 && hourUNIX > commuteUNIX + 1800);
-      });
-      console.log(arrivalHour)
-      statusArr.push(arrivalHour.icon);
-    });
-    console.log("WHAT IS GOIN ON???", statusArr);
-    this.setState({ status: statusArr });
+                // check the coresponding place's weather
+                if (acc === 'clear') {
+                  if (arrivalHour.icon === 'rain' || arrivalHour.icon === 'snow') {
+                    return arrivalHour.icon;
+                  }
+                  return acc;
+                } else if (acc === 'rain') {
+                  if (arrivalHour.icon === 'snow') {
+                    return arrivalHour.icon;
+                  }
+                  return acc;
+                }
+                return acc;
+              }
+            }, 'clear'),
+          });
+        }
+      },
+    );
   }
 
   render() {
